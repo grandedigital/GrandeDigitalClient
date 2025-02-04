16764b7401befcd1444084381907ca4b459d472e
tevfikkdurmus <tevfikk.durmus@gmail.com>
2025-02-04 15:10:56 +0300

    posts animations finished

--- src/components/posts/index.tsx
+++ src/components/posts/index.tsx
@@ -1,16 +1,57 @@
+"use client";
+import { motion } from "motion/react";
+import { useInView } from "motion/react";
+import { useRef, useState } from "react";
 import Link from "next/link";
 
 export default function Posts() {
+  const [isAnimationsPassive, setIsAnimationsPassive] = useState(
+    window.innerWidth < 1217
+  );
+
+  const titleAndLinkRef = useRef(null);
+  const postsRef = useRef(null);
+
+  const longTextViewArea = useInView(titleAndLinkRef, {
+    once: true,
+    amount: 0.2,
+  });
+
+  const postsViewArea = useInView(postsRef, {
+    once: true,
+    amount: 0.2,
+  });
+
   return (
     <section className="pb-0 max-2xl:pt-[90px] max-mdx:pt-[75px] max-md:pt-[50px]">
       <div className="custom-container">
-        <div className="row mb-[4%]">
-          <div className="px-[15px] text-center w-full max-w-full md:text-left md:flex-[0_0_auto] md:w-1/2 max-md:mb-[20px]">
+        <div ref={titleAndLinkRef} className="row mb-[4%]">
+          <motion.div
+            initial={!isAnimationsPassive && { translateY: 30, opacity: 0 }}
+            animate={
+              !isAnimationsPassive && {
+                translateY: longTextViewArea ? 0 : 30,
+                opacity: longTextViewArea ? 1 : 0,
+              }
+            }
+            transition={{ duration: 0.6, delay: 0 }}
+            className="px-[15px] text-center w-full max-w-full md:text-left md:flex-[0_0_auto] md:w-1/2 max-md:mb-[20px]"
+          >
             <h2 className="font-semibold tracking-[-3px] text-[--dark-gray2] font-spaceGrotesk mb-0">
               Our journal
             </h2>
-          </div>
-          <div className="px-[15px] text-center w-full max-w-full md:text-right md:flex-[0_0_auto] md:w-1/2">
+          </motion.div>
+          <motion.div
+            initial={!isAnimationsPassive && { translateY: 30, opacity: 0 }}
+            animate={
+              !isAnimationsPassive && {
+                translateY: longTextViewArea ? 0 : 30,
+                opacity: longTextViewArea ? 1 : 0,
+              }
+            }
+            transition={{ duration: 0.6, delay: 0.3 }}
+            className="px-[15px] text-center w-full max-w-full md:text-right md:flex-[0_0_auto] md:w-1/2"
+          >
             <Link
               href={"#"}
               className="group border-b-0 p-0 normal-case relative border-0 tracking-[0] bg-transparent text-[--dark-gray2] rounded-none w-auto transition-all duration-300 ease-in-out font-medium inline-block text-18 leading-[--bs-btn-line-height] text-center align-middle cursor-pointer select-none"
@@ -51,13 +92,33 @@ export default function Posts() {
                 </span>
               </span>
             </Link>
-          </div>
+          </motion.div>
         </div>
-        <div className="row">
+        <div ref={postsRef} className="row">
           <div className="flex-[0_0_auto] w-full max-w-full flex flex-wrap md:p-0">
             <ul className="p-0 m-0 w-full flex flex-wrap">
-              <li className="w-1/3 p-[10px] list-none max-mdx:w-1/2 max-md:p-[10px] max-md:w-1/2 max-xsm:w-full">
-                <div className="rounded-none bg-[--base-color] h-full border-0 overflow-hidden flex min-w-0 flex-col relative text-[--bs-body-color]">
+              <li
+                className="w-1/3 p-[10px] list-none max-mdx:w-1/2 max-md:p-[10px] max-md:w-1/2 max-xsm:w-full"
+                style={{ perspective: "1200px" }}
+              >
+                <motion.div
+                  initial={
+                    !isAnimationsPassive && {
+                      opacity: 0,
+                      rotateX: 50,
+                      scale: 1.1,
+                    }
+                  }
+                  animate={
+                    !isAnimationsPassive && {
+                      opacity: postsViewArea ? 1 : 0,
+                      rotateX: postsViewArea ? 0 : 50,
+                      scale: postsViewArea ? 1 : 1.1,
+                    }
+                  }
+                  transition={{ duration: 0.8, delay: 0.5 }}
+                  className="rounded-none bg-[--base-color] h-full border-0 overflow-hidden flex min-w-0 flex-col relative text-[--bs-body-color]"
+                >
                   <div className="text-inherit relative overflow-hidden p-[15%] z-1 flex-[1_1_auto] max-lgx:p-[10%] group">
                     <img
                       className="absolute left-0 top-0 object-cover w-full h-full opacity-0 invisible transition-all duration-300 ease-[cubic-bezier(.37,0,.63,1)] scale-110 -rotate-3 max-w-full group-hover:opacity-100 group-hover:visible group-hover:transition-all group-hover:duration-400 group-hover:ease-[cubic-bezier(.37,0,.63,1)] group-hover:scale-100 group-hover:rotate-0"
@@ -83,10 +144,30 @@ export default function Posts() {
                       Design can speak the in tongue of art force of commerce.
                     </Link>
                   </div>
-                </div>
+                </motion.div>
               </li>
-              <li className="w-1/3 p-[10px] list-none max-mdx:w-1/2 max-md:p-[10px] max-md:w-1/2 max-xsm:w-full">
-                <div className="rounded-none bg-[--base-color] h-full border-0 overflow-hidden flex min-w-0 flex-col relative text-[--bs-body-color]">
+              <li
+                className="w-1/3 p-[10px] list-none max-mdx:w-1/2 max-md:p-[10px] max-md:w-1/2 max-xsm:w-full"
+                style={{ perspective: "1200px" }}
+              >
+                <motion.div
+                  initial={
+                    !isAnimationsPassive && {
+                      opacity: 0,
+                      rotateX: 50,
+                      scale: 1.1,
+                    }
+                  }
+                  animate={
+                    !isAnimationsPassive && {
+                      opacity: postsViewArea ? 1 : 0,
+                      rotateX: postsViewArea ? 0 : 50,
+                      scale: postsViewArea ? 1 : 1.1,
+                    }
+                  }
+                  transition={{ duration: 0.8, delay: 0.8 }}
+                  className="rounded-none bg-[--base-color] h-full border-0 overflow-hidden flex min-w-0 flex-col relative text-[--bs-body-color]"
+                >
                   <div className="text-inherit relative overflow-hidden p-[15%] z-1 flex-[1_1_auto] max-lgx:p-[10%] group">
                     <img
                       className="absolute left-0 top-0 object-cover w-full h-full opacity-0 invisible transition-all duration-300 ease-[cubic-bezier(.37,0,.63,1)] scale-110 -rotate-3 max-w-full group-hover:opacity-100 group-hover:visible group-hover:transition-all group-hover:duration-400 group-hover:ease-[cubic-bezier(.37,0,.63,1)] group-hover:scale-100 group-hover:rotate-0"
@@ -112,10 +193,30 @@ export default function Posts() {
                       Simplicity is process the obvious and adding meaningful.
                     </Link>
                   </div>
-                </div>
+                </motion.div>
               </li>
-              <li className="w-1/3 p-[10px] list-none max-mdx:w-1/2 max-md:p-[10px] max-md:w-1/2 max-xsm:w-full">
-                <div className="rounded-none bg-[--base-color] h-full border-0 overflow-hidden flex min-w-0 flex-col relative text-[--bs-body-color]">
+              <li
+                className="w-1/3 p-[10px] list-none max-mdx:w-1/2 max-md:p-[10px] max-md:w-1/2 max-xsm:w-full"
+                style={{ perspective: "1200px" }}
+              >
+                <motion.div
+                  initial={
+                    !isAnimationsPassive && {
+                      opacity: 0,
+                      rotateX: 50,
+                      scale: 1.1,
+                    }
+                  }
+                  animate={
+                    !isAnimationsPassive && {
+                      opacity: postsViewArea ? 1 : 0,
+                      rotateX: postsViewArea ? 0 : 50,
+                      scale: postsViewArea ? 1 : 1.1,
+                    }
+                  }
+                  transition={{ duration: 0.8, delay: 1.1 }}
+                  className="rounded-none bg-[--base-color] h-full border-0 overflow-hidden flex min-w-0 flex-col relative text-[--bs-body-color]"
+                >
                   <div className="text-inherit relative overflow-hidden p-[15%] z-1 flex-[1_1_auto] max-lgx:p-[10%] group">
                     <img
                       className="absolute left-0 top-0 object-cover w-full h-full opacity-0 invisible transition-all duration-300 ease-[cubic-bezier(.37,0,.63,1)] scale-110 -rotate-3 max-w-full group-hover:opacity-100 group-hover:visible group-hover:transition-all group-hover:duration-400 group-hover:ease-[cubic-bezier(.37,0,.63,1)] group-hover:scale-100 group-hover:rotate-0"
@@ -141,7 +242,7 @@ export default function Posts() {
                       Good design obvious, great design is more transparent.
                     </Link>
                   </div>
-                </div>
+                </motion.div>
               </li>
             </ul>
           </div>
