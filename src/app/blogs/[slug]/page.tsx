6ceb26b12b497030247efff62d3eedfaea1889a0
tevfikkdurmus <tevfikk.durmus@gmail.com>
2025-02-08 15:46:58 +0300

    fullphoto1 added

--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -1,9 +1,11 @@
+import FullPhoto1 from "@/components/blog/fullPhoto1";
 import Header from "@/components/blog/header";
 
 export default function Page() {
   return (
     <div>
       <Header />
+      <FullPhoto1 />
     </div>
   );
 }
---
+++ src/components/blog/fullPhoto1.tsx
@@ -0,0 +1,17 @@
+export default function FullPhoto1() {
+  return (
+    <section className="px-[13%] py-0 max-lgx:px-[4%] max-md:px-0">
+      <div className="px-[15px] w-full mx-auto">
+        <div className="row justify-center">
+          <div className="px-[15px] flex-[0_0_auto] w-full max-w-full">
+            <img
+              src="/images/blog-single-creative-01.jpg"
+              alt=""
+              className="w-full max-w-full h-auto"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
